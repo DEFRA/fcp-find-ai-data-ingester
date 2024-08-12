02b2f0ebffcf9d3a62e45623ef54ccad17e0fe17
Foysal Mohammed <MohammedFoysal@users.noreply.github.com>
2024-08-12 13:07:37 +0100

    FFAFGB-102: Store correct grant links (#10)

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,10 @@
+process.env.LOG_LEVEL = 'error'
+
 jest.mock('joi', () => ({
   string: jest.fn().mockReturnValue({
-    optional: jest.fn()
+    optional: jest.fn(),
+    default: jest.fn(),
+    required: jest.fn()
   }),
   object: jest.fn().mockReturnValue({
     validate: jest.fn().mockReturnValue({
